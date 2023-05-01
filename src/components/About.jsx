801b36a028c59789c863b59982439e6538d82c7a
al1yew <vasifja@code.edu.az>
2023-05-01 22:24:02 +0400

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
                   Aspernatur quisquam harum nam cumque temporibus explicabo
                   dolorum sapiente odio unde dolor?
                </p>
-               <a href="#" className="btn">
+               <a href="#about" className="btn">
                   read more
                </a>
             </article>
